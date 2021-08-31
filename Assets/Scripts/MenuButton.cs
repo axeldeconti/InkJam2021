93b812e78c51d8796617daf148124469d459c16e
Axel <axeldeconti@gmail.com>
2021-08-31 18:32:45 +0200

    Hover on sum buttons

--- Assets/Scripts/MenuButton.cs
+++ Assets/Scripts/MenuButton.cs
@@ -17,6 +17,11 @@ public class MenuButton : MonoBehaviour
     [SerializeField] private float _closeTime = 1;
     [SerializeField] private Ease _closeEase = Ease.OutBounce;
 
+    [Header("SFX")]
+    [SerializeField] private AudioClip _SFXOpen;
+    [SerializeField] private AudioClip _SFXClose;
+    [SerializeField] private AudioClip _click;
+
     private TweenerCore<float, float, FloatOptions> _tween;
 
     private void Start()
@@ -42,6 +47,8 @@ public class MenuButton : MonoBehaviour
 
         _tween = _image.DOFillAmount(1, _openTime).SetEase(_openEase);
         _text.color = new Color(0.1960784f, 0.1960784f, 0.1960784f);
+
+        SFXManager.Instance.Play2(_SFXOpen);
     }
 
     public void OnPointerExit()
@@ -51,5 +58,13 @@ public class MenuButton : MonoBehaviour
 
         _tween = _image.DOFillAmount(0, _closeTime).SetEase(_closeEase);
         _text.color = Color.white;
+
+        SFXManager.Instance.Play2(_SFXClose);
     }
+
+    public void PlayClickSound()
+    {
+        SFXManager.Instance.Play(_click);
+    }
+
 }
